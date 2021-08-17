f1c964c88e87883e2d0b411ef8be454d982dac79
ppanigrahisau <82024760+ppanigrahisau@users.noreply.github.com>
2021-08-17 13:09:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var hypnoticBall, database;
+var gameState = 0;
+var playerCount = 0;
+
+var form, game, player
+ 
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game = new Game()
+  game.getState()
+  game.start()
+  
+  
+}
+
+function draw(){
+  background("white");
+  
+    
+    drawSprites();
+  
+}
+
+
+
